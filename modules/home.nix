efaf4e1d2e90a07a021878ca4910c348a66facb6
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2025-07-30 13:38:40 +1200

    feat: add vlc to home manager

--- modules/home.nix
+++ modules/home.nix
@@ -33,6 +33,7 @@
     pkgs.git
     pkgs.btop
     pkgs.fastfetch
+    pkgs.vlc
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
